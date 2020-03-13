bfe3a3a2c11795e0b883137faad7e0cd730e3330
reget <re.masterzero@gmail.com>
2020-03-13 15:14:01 +0800

    添加Deck.models

--- Deck/models.py
+++ Deck/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from Login.models import User
 
-# Create your models here.
+
+class Deck(models.Model):
+    deck_id = models.IntegerField(primary_key=True)
+    # 卡组名
+    name = models.CharField(max_length=30)
+    # 卡组内卡片总数
+    amount = models.IntegerField(default=0)
+    # 创建者
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    # 管理员
+    admins = models.ManyToManyField(User, related_name='AdminsToDeck')
+    # 卡组用户
+    staffs = models.ManyToManyField(User, related_name='StaffsToDeck')
+    c_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return u'<%s,%d>' % (self.name, self.deck_id)
