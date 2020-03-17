24d454b7ecd39fa5fcbae9c345fe03b641476cc2
reget <re.masterzero@gmail.com>
2020-03-17 15:44:54 +0800

    Revert "添加deck功能，修改card功能"
    This reverts commit 326dd011

--- Card/urls.py
+++ Card/urls.py
@@ -5,6 +5,8 @@ app_name = 'Card'
 urlpatterns = [
     path('ShowCards', views.get_cards),
     path('addCard', views.add_card),
+    path('ModifyCard', views.modify_card),
+    path('RemoveCard', views.remove_card),
     path('GetMemoryCar', views.get_memory_card),
     path('RememberCard', views.remember_card),
     path('ForgetCard', views.forget_card)
