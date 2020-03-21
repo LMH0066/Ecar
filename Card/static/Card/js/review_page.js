68b28d8c297da9376ddb4fb5cf41dc254476f8eb
liminghong <35798463+475636679@users.noreply.github.com>
2020-03-21 12:04:16 +0800

    添加翻转效果

--- Card/static/Card/js/review_page.js
+++ Card/static/Card/js/review_page.js
@@ -4,4 +4,21 @@ $(function () {
     review_card_a.attr("aria-expanded", true);
     review_card_a.attr("data-active", true);
     $('.cards').commentCards();
+    document.onkeydown = function () {
+        keyMonitor()
+    };
 });
+
+
+function keyMonitor() {
+    if (event.keyCode === 32) {
+        let $card_current = $('.card--current');
+        if ($card_current.children('.card-front').hasClass('showBack')) {
+            $card_current.children('.card-front').removeClass('showBack');
+            $card_current.children('.card-back').removeClass('showFront');
+        } else {
+            $card_current.children('.card-front').addClass('showBack');
+            $card_current.children('.card-back').addClass('showFront');
+        }
+    }
+}
