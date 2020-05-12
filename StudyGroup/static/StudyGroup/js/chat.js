67ff2f93a5127d4552b83d78504f1b1150918b86
LMH0066 <475636679@qq.com>
2020-05-12 20:43:24 +0800

    修改websocket相关bug

--- StudyGroup/static/StudyGroup/js/chat.js
+++ StudyGroup/static/StudyGroup/js/chat.js
@@ -30,47 +30,47 @@ $('.hamburger, .chat-system .chat-box .chat-not-selected p').on('click', functio
 let updateVal;
 
 // 第一次获取聊天信息
-function getChats(findChat) {
-    if (updateVal) {
-        clearInterval(updateVal);
-    }
-    let group_id = findChat.split('_')[1];
-    let form_data = new FormData();
-    form_data.append('group_id', group_id);
-    $.ajax({
-        url: "/group/GetChats",
-        type: "POST",
-        data: form_data,
-        cache: false,
-        contentType: false,
-        processData: false,
-        dataType: "json",
-        success: function (ret) {
-            if (ret['status']) {
-                // 清空
-                let chat_container = $('#chat-conversation-box-scroll .chat[data-chat = ' + findChat + ']');
-                chat_container.empty();
-                // console.log(chat_container);
-                let data = ret['data'];
-                for (let i = 0; i < data.length; i++) {
-                    let $messageHtml;
-                    if (data[i]['from_is_me']) {
-                        $messageHtml = '<div data-chat-id="' + data[i]['chat_id'] + '" class="bubble me">' + data[i]['content'] + '</div>';
-                    } else {
-                        $messageHtml = '<div data-chat-id="' + data[i]['chat_id'] + '" class="bubble you">' + data[i]['content'] + '</div>';
-                    }
-                    $('.mail-write-box').parents('.chat-system').find('.active-chat').append($messageHtml);
-                }
-                const getScrollContainer = document.querySelector('.chat-conversation-box');
-                getScrollContainer.scrollTop = getScrollContainer.scrollHeight;
-            }
-            updateVal = setInterval(updateChat, 1000);
-        },
-        error: function () {
-            Oops("");
-        }
-    })
-}
+// function getChats(findChat) {
+//     if (updateVal) {
+//         clearInterval(updateVal);
+//     }
+//     let group_id = findChat.split('_')[1];
+//     let form_data = new FormData();
+//     form_data.append('group_id', group_id);
+//     $.ajax({
+//         url: "/group/GetChats",
+//         type: "POST",
+//         data: form_data,
+//         cache: false,
+//         contentType: false,
+//         processData: false,
+//         dataType: "json",
+//         success: function (ret) {
+//             if (ret['status']) {
+//                 // 清空
+//                 let chat_container = $('#chat-conversation-box-scroll .chat[data-chat = ' + findChat + ']');
+//                 chat_container.empty();
+//                 // console.log(chat_container);
+//                 let data = ret['data'];
+//                 for (let i = 0; i < data.length; i++) {
+//                     let $messageHtml;
+//                     if (data[i]['from_is_me']) {
+//                         $messageHtml = '<div data-chat-id="' + data[i]['chat_id'] + '" class="bubble me">' + data[i]['content'] + '</div>';
+//                     } else {
+//                         $messageHtml = '<div data-chat-id="' + data[i]['chat_id'] + '" class="bubble you">' + data[i]['content'] + '</div>';
+//                     }
+//                     $('.mail-write-box').parents('.chat-system').find('.active-chat').append($messageHtml);
+//                 }
+//                 const getScrollContainer = document.querySelector('.chat-conversation-box');
+//                 getScrollContainer.scrollTop = getScrollContainer.scrollHeight;
+//             }
+//             updateVal = setInterval(updateChat, 1000);
+//         },
+//         error: function () {
+//             Oops("");
+//         }
+//     })
+// }
 
 function showChat(div) {
     // console.log($(div).hasClass('active'));
@@ -139,42 +139,42 @@ function sendChat(content, chatInput) {
     // })
 }
 
-function updateChat() {
-    let $chat = $('.active-chat');
-    if ($chat.length > 0) {
-        let chat_id = $chat.children("div:last-child").attr('data-chat-id');
-        let form_data = new FormData();
-        form_data.append('chat_id', chat_id);
-        $.ajax({
-            url: "/group/UpdateChatMessage",
-            type: "POST",
-            data: form_data,
-            cache: false,
-            contentType: false,
-            processData: false,
-            dataType: "json",
-            success: function (ret) {
-                if (ret['status']) {
-                    let data = ret['data'];
-                    for (let i = 0; i < data.length; i++) {
-                        let $messageHtml;
-                        if (data[i]['from_is_me']) {
-                            $messageHtml = '<div data-chat-id="' + data[i]['chat_id'] + '" class="bubble me">' + data[i]['content'] + '</div>';
-                        } else {
-                            $messageHtml = '<div data-chat-id="' + data[i]['chat_id'] + '" class="bubble you">' + data[i]['content'] + '</div>';
-                        }
-                        $('.mail-write-box').parents('.chat-system').find('.active-chat').append($messageHtml);
-                    }
-                    const getScrollContainer = document.querySelector('.chat-conversation-box');
-                    getScrollContainer.scrollTop = getScrollContainer.scrollHeight;
-                }
-            },
-            error: function () {
-                Oops("");
-            }
-        })
-    }
-}
+// function updateChat() {
+//     let $chat = $('.active-chat');
+//     if ($chat.length > 0) {
+//         let chat_id = $chat.children("div:last-child").attr('data-chat-id');
+//         let form_data = new FormData();
+//         form_data.append('chat_id', chat_id);
+//         $.ajax({
+//             url: "/group/UpdateChatMessage",
+//             type: "POST",
+//             data: form_data,
+//             cache: false,
+//             contentType: false,
+//             processData: false,
+//             dataType: "json",
+//             success: function (ret) {
+//                 if (ret['status']) {
+//                     let data = ret['data'];
+//                     for (let i = 0; i < data.length; i++) {
+//                         let $messageHtml;
+//                         if (data[i]['from_is_me']) {
+//                             $messageHtml = '<div data-chat-id="' + data[i]['chat_id'] + '" class="bubble me">' + data[i]['content'] + '</div>';
+//                         } else {
+//                             $messageHtml = '<div data-chat-id="' + data[i]['chat_id'] + '" class="bubble you">' + data[i]['content'] + '</div>';
+//                         }
+//                         $('.mail-write-box').parents('.chat-system').find('.active-chat').append($messageHtml);
+//                     }
+//                     const getScrollContainer = document.querySelector('.chat-conversation-box');
+//                     getScrollContainer.scrollTop = getScrollContainer.scrollHeight;
+//                 }
+//             },
+//             error: function () {
+//                 Oops("");
+//             }
+//         })
+//     }
+// }
 
 
 function WebSocketTest(findChat) {
@@ -187,17 +187,17 @@ function WebSocketTest(findChat) {
         }
         let ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
         // 打开一个 web socket
-        let ws = new WebSocket(ws_scheme + "://" + window.location.host + "/group/ChatWebsocket");
+        let ws = new WebSocket(ws_scheme + "://" + window.location.host + "/group/ChatWebsocket/" + userName);
 
         ws.onopen = function () {
             // Web Socket 已连接上，使用 send() 方法发送数据
-            console.log("准备信息");
+            // console.log("准备信息");
             ws.send(group_id);
-            console.log("发送信息")
+            // console.log("发送信息")
         };
 
         ws.onmessage = function (evt) {
-            console.log("接收信息")
+            // console.log("接收信息")
             let ret = JSON.parse(evt.data);
             // console.log(ret);
             // console.log(chat_container);
